d0165bd0f14dea6d603393ce50556d6f5eef7245
Cepha0Z <154107032+Cepha0Z@users.noreply.github.com>
2025-06-06 15:07:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,32 @@
 import os
-import shutil
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
-from yt_dlp import YoutubeDL
-import zipfile
 
 load_dotenv()
 
 app = Flask(__name__)
+
 sp = Spotify(auth_manager=SpotifyClientCredentials(
     client_id=os.getenv("SPOTIPY_CLIENT_ID"),
     client_secret=os.getenv("SPOTIPY_CLIENT_SECRET")
 ))
 
-DOWNLOAD_FOLDER = 'downloads'
-ZIP_FILE = 'songs.zip'
-
-def get_track_list(album_url):
-    results = sp.album_tracks(album_url)
-    return [f"{track['name']} {track['artists'][0]['name']}" for track in results['items']]
-
-def download_mp3s(tracks):
-    if os.path.exists(DOWNLOAD_FOLDER):
-        shutil.rmtree(DOWNLOAD_FOLDER)
-    os.makedirs(DOWNLOAD_FOLDER)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,
-        'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-        }],
-    }
-
-    with YoutubeDL(ydl_opts) as ydl:
-        for track in tracks:
-            print(f"Downloading: {track}")
-            ydl.download([f"ytsearch1:{track}"])
-
-def zip_songs():
-    if os.path.exists(ZIP_FILE):
-        os.remove(ZIP_FILE)
-
-    with zipfile.ZipFile(ZIP_FILE, 'w') as zipf:
-        for file in os.listdir(DOWNLOAD_FOLDER):
-            if file.endswith('.mp3'):
-                zipf.write(os.path.join(DOWNLOAD_FOLDER, file), arcname=file)
+def get_track_list(playlist_url):
+    playlist_id = playlist_url.split("/")[-1].split("?")[0]
+    results = sp.playlist_tracks(playlist_id)
+    return [f"{item['track']['name']} {item['track']['artists'][0]['name']}" for item in results['items']]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        album_url = request.form['url']
-        tracks = get_track_list(album_url)
-        download_mp3s(tracks)
-        zip_songs()
-        return send_file(ZIP_FILE, as_attachment=True)
+        playlist_url = request.form['url']
+        tracks = get_track_list(playlist_url)
+        return render_template('index.html', tracks=tracks)
 
-    return render_template('index.html')
+    return render_template('index.html', tracks=None)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
